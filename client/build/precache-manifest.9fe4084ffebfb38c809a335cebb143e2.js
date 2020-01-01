2242e8a3dd2f23c8b55790836977e49ec924302d
vagrant <gertsberg@hotmail.com>
2020-01-01 15:29:55 -0800

    test

---
+++ client/build/precache-manifest.9fe4084ffebfb38c809a335cebb143e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c58981bd2115c441ca8a1a0cb2cbb75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cd2edde41359caf8fcf",
+    "url": "/static/css/main.fab71d29.chunk.css"
+  },
+  {
+    "revision": "b9c2b025e20d2c98828c",
+    "url": "/static/js/2.19d9d353.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/static/js/2.19d9d353.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cd2edde41359caf8fcf",
+    "url": "/static/js/main.66bb55fb.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -4,15 +4,19 @@ const apiRouter = require("./routes/apiRoute");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 8080;
+const path = require("path");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api",apiRouter);
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.get("/",(req,res)=>{
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 })
+
 io.on("connection",(socket)=>{
     console.log("a user connected");
     socket.on("disconnect",()=>{
